0c2f561a4cfb55ab2ebdf97208768712681892eb
Vale <oirelav95@gmail.com>
2022-12-08 15:44:51 +0100

    aggiungo componente AppHero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+    data() {
+        return {
+            image: 'img/tramonto.jpg',
+        }
+    }
+
+}
+</script>
+
+<template>
+    <img :src="image">
+  </template>
